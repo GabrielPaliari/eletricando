2599d391bce42cde46176918c41038ae926eb42d
GabrielPaliari <gabriel.paliari@gmail.com>
2023-05-17 20:57:46 -0300

    delete previous project and started new one

--- Assets/_Scripts/BuildingScriptable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewBuilding", menuName = "ScriptableObjects/NewBuilding")]
-public class BuildingScriptable : ScriptableObject{
-    [SerializeField] public GameObject _initialPrefab;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# eletricando
-Jogo no estilo factorio focado no ensino e aprendizagem dos conceitos básicos de elétrica com um foco nas necessidades da humanidade. Feito em unity por brasileires.
