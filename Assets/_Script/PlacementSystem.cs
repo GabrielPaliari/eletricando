896adc9c40addab876ae9b239edaa8da94b96ab8
GabrielPaliari <gabriel.paliari@gmail.com>
2023-05-17 22:46:22 -0300

    Basic Placement Logic - Tutorial Part 3

--- Assets/_Script/PlacementSystem.cs
+++ Assets/_Script/PlacementSystem.cs
@@ -8,15 +8,63 @@ public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
     private GameObject mouseIndicator, cellIndicator;
-
     [SerializeField]
     private InputManager inputManager;
-
     [SerializeField]
     private Grid grid;
 
+    [SerializeField]
+    private ObjectDatabaseSO database;
+    private int selectedObjectIndex = -1;
+
+    [SerializeField]
+    private GameObject gridVisualiztion;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if (selectedObjectIndex == -1)
+        {
+            Debug.LogError($"No ID found {ID}");
+            return;
+        }
+
+        gridVisualiztion.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.IsPointerOverUI()) { return; }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject gameObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        gameObject.transform.position = grid.CellToWorld(gridPosition);
+
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualiztion.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
     private void Update()
     {
+        if (selectedObjectIndex == -1) { return; }
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
