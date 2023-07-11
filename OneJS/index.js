ac806c08d5a94ecf55ed944e974ef7e99ccb2398
GabrielPaliari <gabriel.paliari@gmail.com>
2023-07-11 19:28:32 -0300

    feat(onejs): created menu with onejs

--- OneJS/index.js
+++ OneJS/index.js
@@ -2,20 +2,35 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var preact_1 = require("preact");
 var hooks_1 = require("preact/hooks");
+var placementSystem = require("placementSystem");
+var wireSystem = require("wireSystem");
+var MenuComp = function (props) {
+    var defaultInnerStyle = {
+        borderRadius: 4,
+        paddingBottom: "100%",
+        backgroundImage: __dirname + "/assets/" + props.imgFile,
+        justifyContent: "Center",
+        alignItems: "Center",
+        color: "white",
+    };
+    return ((0, preact_1.h)("div", { class: "rounded bg-white m-3 w-16", onClick: props.onClick },
+        (0, preact_1.h)("div", { style: defaultInnerStyle })));
+};
 var App = function () {
     var ref = (0, hooks_1.useRef)();
-    return ((0, preact_1.h)("div", { ref: ref },
-        (0, preact_1.h)("div", { class: "max-w-xl h-full font-sans bg-indigo-600" },
-            (0, preact_1.h)("nav", { className: "flex flex-row" }, [
-                ["Teste", "/dashboard"],
-                ["Team", "/team"],
-                ["Projects", "/projects"],
-                ["", ""],
-                ["Reports", "/reports"],
-            ].map(function (_a) {
-                var title = _a[0], url = _a[1];
-                return title ? ((0, preact_1.h)("div", { class: "basis-1/5 rounded bg-white text-lg p-3 m-3" },
-                    (0, preact_1.h)("img", { src: __dirname + "/assets/battery-sprite.png" }))) : ((0, preact_1.h)("div", { class: "basis-1/5" }));
-            })))));
+    var navStyle = {
+        borderRadius: 8,
+    };
+    var menuData = [
+        ["wire-sprite.png", function () { return wireSystem.EnterWireMode(); }],
+        ["battery-sprite.png", function () { return placementSystem.StartPlacement(3); }],
+        ["resistor-sprite.png", function () { return placementSystem.StartPlacement(1); }],
+        ["remove-icon.png", function () { return placementSystem.StartRemoving(); }],
+    ];
+    return ((0, preact_1.h)("div", { ref: ref, class: "container mt-auto w-screen" },
+        (0, preact_1.h)("nav", { className: "font-sans bg-indigo-800 flex flex-row mx-auto rounded-lg" }, menuData.map(function (_a) {
+            var imgFile = _a[0], onClick = _a[1];
+            return ((0, preact_1.h)(MenuComp, { imgFile: imgFile, length: menuData.length, onClick: onClick }));
+        }))));
 };
 (0, preact_1.render)((0, preact_1.h)(App, null), document.body);
--- OneJS/index.tsx
+++ OneJS/index.tsx
@@ -1,31 +1,60 @@
 import { h, render } from "preact";
 import { Dom } from "OneJS/Dom";
 import { useRef } from "preact/hooks";
+import { Style } from "preact/jsx";
+
+const placementSystem = require("placementSystem");
+const wireSystem = require("wireSystem");
+
+interface MenuCompProps {
+  imgFile: string;
+  length: number;
+  onClick: () => void;
+}
+
+const MenuComp = (props: MenuCompProps) => {
+  const defaultInnerStyle: Style = {
+    borderRadius: 4,
+    paddingBottom: "100%",
+    backgroundImage: __dirname + "/assets/" + props.imgFile,
+    justifyContent: "Center",
+    alignItems: "Center",
+    color: "white",
+  };
+
+  return (
+    <div class="rounded bg-white m-3 w-16" onClick={props.onClick}>
+      <div style={defaultInnerStyle}></div>
+    </div>
+  );
+};
 
 const App = () => {
   const ref = useRef<Dom>();
 
+  const navStyle: Style = {
+    borderRadius: 8,
+  };
+
+  const menuData: [string, () => void][] = [
+    ["wire-sprite.png", () => wireSystem.EnterWireMode()],
+    ["battery-sprite.png", () => placementSystem.StartPlacement(3)],
+    ["resistor-sprite.png", () => placementSystem.StartPlacement(1)],
+    ["remove-icon.png", () => placementSystem.StartRemoving()],
+  ];
   return (
-    <div ref={ref}>
-      <div class="max-w-xl h-full font-sans bg-indigo-600">
-        <nav className="flex flex-row">
-          {[
-            ["Teste", "/dashboard"],
-            ["Team", "/team"],
-            ["Projects", "/projects"],
-            ["", ""],
-            ["Reports", "/reports"],
-          ].map(([title, url]) => {
-            return title ? (
-              <div class="basis-1/5 rounded bg-white text-lg p-3 m-3">
-                <img src={__dirname + "/assets/battery-sprite.png"} />
-              </div>
-            ) : (
-              <div class="basis-1/5"></div>
-            );
-          })}
-        </nav>
-      </div>
+    <div ref={ref} class="container mt-auto w-screen">
+      <nav className="font-sans bg-indigo-800 flex flex-row mx-auto rounded-lg">
+        {menuData.map(([imgFile, onClick]) => {
+          return (
+            <MenuComp
+              imgFile={imgFile}
+              length={menuData.length}
+              onClick={onClick}
+            ></MenuComp>
+          );
+        })}
+      </nav>
     </div>
   );
 };
